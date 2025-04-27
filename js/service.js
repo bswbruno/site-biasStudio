10ccd6e56d241ff939b25888a01e735c930178c3
bswbruno <brunusuxx@gmail.com>
2025-04-27 12:58:29 -0300

    att

--- js/service.js
+++ js/service.js
@@ -7,7 +7,7 @@ window.services = [
     imageUrl:
       "https://farmaciaprimavita.com.br/wp-content/uploads/2023/06/image2.png",
     duration: "60 min",
-    price: "A consultar"
+    price: "Consulte"
     // price: "R$ 150,00",
   },
   {
@@ -18,7 +18,7 @@ window.services = [
     imageUrl:
       "https://negociosdebeleza.beautyfair.com.br/wp-content/uploads/2024/07/coloracao-para-sobrancelhas-capa.webp",
     duration: "45 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 90,00",
   },
   {
@@ -29,7 +29,7 @@ window.services = [
     imageUrl:
       "https://cdn.awsli.com.br/2500x2500/2749/2749293/produto/269654068/c--1--xcquaa1pl1.jpg",
     duration: "40 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 80,00",
   },
   {
@@ -40,7 +40,7 @@ window.services = [
     imageUrl:
       "https://blog.hanabeauty.com.br/wp-content/uploads/2021/12/Modelo-com-extensao-de-cilios.jpeg",
     duration: "90-120 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 180,00 - R$ 280,00",
   },
   {
@@ -51,7 +51,7 @@ window.services = [
     imageUrl:
       "https://st.depositphotos.com/1516574/56782/i/450/depositphotos_567827540-stock-photo-caucasian-woman-on-eyelash-lamination.jpg",
     duration: "60 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 150,00",
   },
   {
@@ -62,7 +62,7 @@ window.services = [
     imageUrl:
       "https://softhair.com.br/wp-content/uploads/2019/12/313501-como-se-depilar-em-casa-sozinha-6-dicas-para-usar-cera-sem-medo.jpg",
     duration: "30-60 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 50,00 - R$ 120,00",
   },
   {
@@ -73,7 +73,7 @@ window.services = [
     imageUrl:
       "https://espacobe.com.br/wp-content/uploads/2020/12/o-que-e-depilacao-egipcia-1110x600.jpg",
     duration: "30 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 60,00",
   },
   {
@@ -84,7 +84,7 @@ window.services = [
     imageUrl:
       "https://www.ensofill.com.br/wp-content/uploads/2024/06/Blog-Ensofill-Microagulhamento.png",
     duration: "60 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 250,00",
   },
   {
@@ -95,7 +95,7 @@ window.services = [
     imageUrl:
       "https://www.dermaclub.com.br/on/demandware.static/-/Sites-dermaclub-br-Library/pt_BR/dw9b42face/peeling-diamante.jpg",
     duration: "50 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 180,00",
   },
   {
@@ -106,7 +106,7 @@ window.services = [
     imageUrl:
       "https://priscilacartaxodermato.com.br/wp-content/uploads/2021/05/dermaplaning-bh-1.jpg",
     duration: "45 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 150,00",
   },
   {
@@ -117,7 +117,7 @@ window.services = [
     imageUrl:
       "https://blog.adcosprofissional.com.br/wp-content/uploads/2021/06/2_Kit-para-limpeza-de-pele-potencialize-seus-resultados-com-uma-pele-bem-preparada-1.jpg",
     duration: "75 min",
-    price: "A consultar"
+    price: "Consulte"
     //price: "R$ 170,00",
   },
   {
@@ -129,6 +129,7 @@ window.services = [
       "https://www.botopremium.com.br/wp-content/uploads/2024/05/jatodeplasma-1920w.webp",
     duration: "60 min",
     //price: "R$ 280,00",
-    price: "A consultar"
+    price: "Consulte"
+
   },
 ];
